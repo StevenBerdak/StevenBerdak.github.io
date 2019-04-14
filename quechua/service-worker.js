fa52c191d4a222c8b6cb7d52500e56979847e07f
Steven Berdak <stevenberdak@gmail.com>
2019-04-14 00:51:19 -0700

    Quechua app

--- quechua/service-worker.js
+++ quechua/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.066a341fab97b150e0d89dbbeb476a3d.js"
+  "./quechua/precache-manifest.fc39ebf7c187cddfd025fcfe3e90e8ad.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./quechua/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
